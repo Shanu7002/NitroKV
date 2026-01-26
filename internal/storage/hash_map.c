74134560676e2440d07c29ab4ce7430265b315bd
Edu Muller <eduardoaum2007@gmail.com>
2026-01-26 19:02:09 -0300

    refactor: little refactor in some small problems

--- internal/storage/hash_map.c
+++ internal/storage/hash_map.c
@@ -5,16 +5,20 @@
 
 // Helper
 size_t hash(HashMap *table, const char *key) {
-    size_t hash = 0;
+    const size_t FNV_offset_basis = 14695981039346656037ULL;
+    const size_t FNV_prime = 1099511628211ULL;
+
+    size_t hash_val = FNV_offset_basis;
 
     for (size_t i = 0; key[i] != '\0'; i++) {
-        hash += (unsigned char)key[i];
+        hash_val ^= (unsigned char)key[i];
+        hash_val *= FNV_prime;
     }
-    return hash % table->size;
+
+    return hash_val % table->size;
 }
 
 // Funcions
-
 HashMap *create_table(size_t size) {
     HashMap *map = malloc(sizeof(HashMap));
     if (!map) return NULL;
@@ -33,24 +37,35 @@ HashMap *create_table(size_t size) {
 
 void set_item(HashMap *table, const char *key, const char *value) {
     size_t index = hash(table, key);
-
     Entry *current = table->buckets[index];
 
     // TODO: function to double the array size and realloc all the itens
     while (current) {
         if (strcmp(current->key, key) == 0) {
+            char *new_val = strdup(value);
+            if(!new_val) return;
+
             free(current->value);
-            current->value = strdup(value);
+            current->value = new_val;
             return;
         }
         current = current->next;
     }
 
     Entry *new_entry = malloc(sizeof(Entry));
+    if (!new_entry) return;
+
     new_entry->key = strdup(key);
     new_entry->value = strdup(value);
-    new_entry->next = table->buckets[index];
-    
+
+    if (!new_entry->key || !new_entry->value) {
+        free(new_entry->key);
+        free(new_entry->value);
+        free(new_entry);
+        return;
+    }
+
+    new_entry->next = table->buckets[index];    
     table->buckets[index] = new_entry;
     table->count++;
 }
