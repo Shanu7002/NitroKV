5810faea797c6f0695bc868683d9d5e135ac7e7c
Edu Muller <eduardoaum2007@gmail.com>
2026-01-31 12:03:44 -0300

    refactor: protocols refactored for protocol.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,18 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
-	"nitrokv/internal/engine"
 	"nitrokv/internal/network"
-	"strings"
 )
 
 func main() {
 	server := network.NewServer(":6379")
 
-	newMap, err := engine.New(16)
-	if err != nil {
-		log.Fatal()
-	}
+	proto := network.NewProtocolManager()
 
 	go func() {
 		for msg := range server.Message() {
@@ -22,60 +17,7 @@ func main() {
 			if len(msg.Payload) == 0 {
 				continue
 			}
-
-			text := strings.TrimSpace(string(msg.Payload))
-			parts := strings.Fields(text)
-			if len(parts) == 0 {
-				continue
-			}
-
-			command := strings.ToUpper(parts[0])
-			switch command {
-			case "SET":
-				if len(parts) < 3 {
-					fmt.Fprintln(msg.Conn, "ERR: SET requires key and value.")
-					continue
-				}
-
-				key, value := parts[1], parts[2]
-				newMap.Set(key, value)
-				fmt.Fprintf(msg.Conn, "OK: key '%s' was set with value '%s'\n", key, value)
-			case "GET":
-				if len(parts) < 2 {
-					fmt.Fprintf(msg.Conn, "ERR: GET requires a key.\n")
-					continue
-				}
-				key := parts[1]
-				res, status := newMap.Get(key)
-				if status == false {
-					fmt.Fprintf(msg.Conn, "ERR: Key not found!\n")
-					continue
-				}
-				fmt.Fprintf(msg.Conn, "OK: Value: %s\n", res)
-			case "REMOVE":
-				if len(parts) < 2 {
-					fmt.Fprintf(msg.Conn, "ERR: REMOVE requires a key\n")
-					continue
-				}
-				key := parts[1]
-
-				if _, ok := newMap.Get(key); ok {
-					newMap.Remove(key)
-					fmt.Fprintf(msg.Conn, "OK: key '%s' was sucessfully removed!\n", key)
-				} else {
-					fmt.Fprintf(msg.Conn, "ERR: Key not found!\n")
-					continue
-				}
-			case "CLOSE":
-				fmt.Fprintf(msg.Conn, "OK: Server-wide map destroyed. Connection closing.\n")
-				newMap.Close()
-				msg.Conn.Close()
-			case "QUIT":
-				fmt.Fprintf(msg.Conn, "Goodbye!\n")
-				msg.Conn.Close()
-			default:
-				fmt.Println("Sorry, this function do not exist.")
-			}
+			proto.HandleCommand(msg)
 		}
 	}()
 
--- internal/network/protocol.go
+++ internal/network/protocol.go
@@ -1,1 +1,107 @@
 package network
+
+import (
+	"fmt"
+	"nitrokv/internal/engine"
+	"strings"
+	"sync"
+)
+
+type ProtocolManager struct {
+	dbs      map[string]*engine.Engine
+	sessions map[string]string
+	mu       sync.RWMutex
+}
+
+func NewProtocolManager() *ProtocolManager {
+	return &ProtocolManager{
+		dbs:      make(map[string]*engine.Engine),
+		sessions: make(map[string]string),
+	}
+}
+
+func (p *ProtocolManager) HandleCommand(msg Message) {
+	text := strings.TrimSpace(string(msg.Payload))
+	parts := strings.Fields(text)
+	if len(parts) == 0 {
+		return
+	}
+
+	command := strings.ToUpper(parts[0])
+	switch command {
+	case "REGISTER":
+	case "LOGIN":
+	case "SET":
+		if len(parts) < 3 {
+			fmt.Fprintln(msg.Conn, "ERR: SET requires key and value.")
+			return
+		}
+
+		key, value := parts[1], parts[2]
+		p.handleSet(msg, key, value)
+		fmt.Fprintf(msg.Conn, "OK: key '%s' was set with value '%s'\n", key, value)
+	case "GET":
+		if len(parts) < 2 {
+			fmt.Fprintf(msg.Conn, "ERR: GET requires a key.\n")
+			return
+		}
+		key := parts[1]
+		res, status := p.handleGet(msg, key)
+		if status == false {
+			fmt.Fprintf(msg.Conn, "ERR: Key not found!\n")
+			return
+		}
+		fmt.Fprintf(msg.Conn, "OK: Value: %s\n", res)
+	case "REMOVE":
+		if len(parts) < 2 {
+			fmt.Fprintf(msg.Conn, "ERR: REMOVE requires a key\n")
+			return
+		}
+		key := parts[1]
+
+		if _, ok := p.handleGet(msg, key); ok {
+			p.handleRemove(msg, key)
+			fmt.Fprintf(msg.Conn, "OK: key '%s' was sucessfully removed!\n", key)
+		} else {
+			fmt.Fprintf(msg.Conn, "ERR: Key not found!\n")
+			return
+		}
+	case "CLOSE":
+		fmt.Fprintf(msg.Conn, "OK: Server-wide map destroyed. Connection closing.\n")
+		p.handleClose(msg)
+		msg.Conn.Close()
+	case "QUIT":
+		fmt.Fprintf(msg.Conn, "Goodbye!\n")
+		msg.Conn.Close()
+	default:
+		fmt.Println("Sorry, this function do not exist.")
+	}
+}
+
+func (p *ProtocolManager) handleRegister(msg Message, parts string) {
+
+}
+
+func (p *ProtocolManager) handleLogin(msg Message, parts string) {
+
+}
+
+func (p *ProtocolManager) handleSet(msg Message, key string, value string) {
+
+}
+
+func (p *ProtocolManager) handleGet(msg Message, key string) (string, bool) {
+	return key, true
+}
+
+func (p *ProtocolManager) handleRemove(msg Message, key string) {
+
+}
+
+func (p *ProtocolManager) handleQuit(msg Message) {
+
+}
+
+func (p *ProtocolManager) handleClose(msg Message) {
+
+}
